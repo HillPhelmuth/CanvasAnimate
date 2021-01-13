046148626236cc220d973aae3500f4cc3fbb9841
HillPhelmuth <46843652+HillPhelmuth@users.noreply.github.com>
2021-01-13 00:08:02 -0600

    Add a couple images as element examples using burtons

---
+++ AnimateCanvas/Pages/ImagesAsElementExamples.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimateCanvas.Pages
+{
+    public partial class ImagesAsElementExamples
+    {
+        private List<string> actionLog = new();
+        private void HandleAction(string action)
+        {
+            actionLog.Add(action);
+            if (actionLog.Count > 30)
+                actionLog.RemoveAt(0);
+            StateHasChanged();
+        }
+    }
+}
---
+++ CanvasAnimateCSharp/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CanvasAnimateCSharp
+{
+    public static class Extensions
+    {
+        //public static IServiceCollection AddCanvaServiceCollection(this IServiceCollection service)
+        //{
+        //    service.AddScoped<>()
+        //}
+    }
+}
